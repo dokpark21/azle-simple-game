86d5f534ced6c184d15efb82bce34595302bd824
BEB 09 박상현 <psh03225@naver.com>
2023-10-19 23:46:08 +0900

    Update : token

--- token/src/index.ts
+++ token/src/index.ts
@@ -75,6 +75,8 @@ function _transferFrom(from: text, to: text, amount: nat64): bool {
    * 전송 후 allowance를 갱신하는 것을 잊지 마세요!
    */
 
+  
+
   return true;
 }
 
@@ -147,7 +149,14 @@ export default Canister({
      * getAccountByAddress() 함수를 사용하세요.
      * state에 사용자 정보가 없는 경우, 0을 반환합니다.
      */
-    return 0n;
+
+    const accountOpt= getAccountByAddress(address);
+
+    if ('None' in accountOpt) {
+      return 0n;
+    }
+    
+    return accountOpt.Some.balance;
   }),
 
   transfer: update([text, nat64], bool, (to, amount) => {
@@ -155,21 +164,96 @@ export default Canister({
      * TO-DO: 토큰을 전송합니다.
      * getAccountByAddress() 함수를 사용하세요.
      */
+    const fromAddress = getCaller();
+    const fromAccountOpt = getAccountByAddress(fromAddress);
+    let toAccount;
+    if ('None' in fromAccountOpt){
+      throw new Error("No From Account");
+    }
+    const fromAccount = fromAccountOpt.some;
+
+
+    const toAccountOpt= getAccountByAddress(to);
+    if ('None' in fromAccountOpt){
+      const newToAccount: typeof Account = {
+        address : to,
+        balance : 0n,
+        allowance : [],
+      }  
+      toAccount = insertAccount(to, newToAccount);
+    }else {
+      toAccount = toAccountOpt.some;
+    }
+
+    if (!fromAccount || fromAccount.balance < amount) {
+      return false;
+    }
+
+    fromAccount.balance -= amount;
+    toAccount.balance += amount;
+
+    //state에서 계좌의 상태를 가져와서 상태를 바꿔주고 다시 InsertAccount를 통해서 반영해준다.
+    insertAccount(fromAddress, fromAccount);
+    insertAccount(to, toAccount);
 
     return true;
   }),
 
   approve: update([text, nat64], bool, (spender, amount) => {
+    // 제 3자에게 내가 가지고 있는 토큰의 amount만큼 사용가능하도록 승인
     /*
      * TO-DO: 토큰을 approve 합니다.
      * 기존에 owner가 spender에게 토큰을 approve한 경우, 기존의 값을 덮어 씌워야 합니다.
      */
+    
+    // owner account 계정 가져오기
+    const ownerAddress = getCaller();
+    const ownerAccountOpt = getAccountByAddress(ownerAddress);
+    let spenderAccount;
+    if ('None' in ownerAccountOpt){
+      throw new Error("No Owner Account");
+    }
+    const fromAccount = ownerAccountOpt.some;
+
+    // spender account 가져오기
+        const spenderAccountOpt= getAccountByAddress(spender);
+    if ('None' in spenderAccountOpt){
+      const newToAccount: typeof Account = {
+        address : spender,
+        balance : 0n,
+        allowance : [],
+      }  
+      spenderAccount = insertAccount(spender, newToAccount);
+    }else {
+      spenderAccount = spenderAccountOpt.some;
+    }
+
+    if (!fromAccount || fromAccount.balance < amount) {
+      return false;
+    }
+
+    // 구현 필요!!!!!
 
     return true;
   }),
 
+  // owner가 spender에게 얼마를 approve했는지 반환
   allowance: query([text, text], nat64, (owner, spender) => {
-    return _allowance(owner, spender);
+    const ownerAddress = getCaller();
+    const ownerAccountOpt = getAccountByAddress(ownerAddress);
+    let spenderAccount;
+    if ('None' in ownerAccountOpt){
+      throw new Error("No Owner Account");
+    }
+    const ownerAccount = ownerAccountOpt.some;
+
+    for (let allowance of ownerAccount){
+      if (allowance.spender == spender){
+        return allowance.amount;
+      }
+    }
+
+    return 0n;
   }),
 
   // owner -> 호출자 에 대한 allowance
@@ -178,6 +262,15 @@ export default Canister({
      * TO-DO: 인자로 주어진 owner가 함수를 호출한 caller에게 토큰을 얼마나 approve 해주었는지 확인합니다.
      * allowanceFrom() 함수는 주로 캐니스터 컨트랙트에서 "사용자가 캐니스터에 얼마나 approve 했는지"(사용자 -> 캐니스터) 확인할 때 사용합니다.
      */
+
+    // 호출자에 대한 allowance 확인이기 때문에 caller를 확인한다
+    const spender = getCaller();
+    const spenderAccountOpt = getAccountByAddress(spender);
+    if ('None' in spenderAccountOpt) {
+      return 0n;
+    }else {
+      return _allowance(owner, spender);
+    }
 
     return 0n;
   }),
